3766116849241832365f9bab97cdc506c24c2ff0
Seb <sdieunidou@users.noreply.github.com>
2020-11-10 19:22:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,24 @@ const client = new CommandoClient({
     disableMentions: 'everyone'
 });
 
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        const eventFunction = require(`./events/${file}`);
+        if (eventFunction.disabled) return;
+
+        const event = eventFunction.event || file.split('.')[0];
+        const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
+        const { once } = eventFunction;
+
+        try {
+            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args));
+        } catch (error) {
+            console.error(error.stack);
+        }
+    });
+});
+
 client.registry
     .registerDefaultTypes()
     .registerGroups([
